7050dd422d211fffc58ae7db82b94628ff9103f8
Christoffer Slettan <christofferslettan@gmail.com>
2018-10-16 12:31:48 +0200

    add simple console app for TC to build

---
+++ src/DoggoApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DoggoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintDoggos();
+        }
+
+        static void PrintDoggos()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(@"
+            _     /)---(\          /~~~\
+            \\   (/ . . \)        /  .. \
+             \\__)-\(*)/         (_,\  |_)
+             \_       (_         /   \@/    /^^^\
+             (___/-(____) _     /      \   / . . \
+                          \\   /  `    |   V\ Y /V
+                           \\/  \   | _\    / - \
+               woof woof    \   /__'|| \\_  |    \
+                             \_____)|_).\_).||(__V
+
+            ");
+            Console.ResetColor();
+        }
+    }
+}
