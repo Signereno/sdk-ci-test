6af25f378c7fe359786af04743c4d55f49e7c37b
Christoffer Slettan <christofferslettan@gmail.com>
2018-11-29 12:29:45 +0100

    stuff

---
+++ src/DoggoApp.Tests/Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace DoggoApp.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public async Task Test()
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync("http://localhost:5000");
+            }
+        }
+    }
+}
